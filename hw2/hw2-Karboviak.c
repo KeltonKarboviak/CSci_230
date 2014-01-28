ef8cbce4ccf52847e6d5c30fd25271f95065d4d8
KeltonKarboviak <kelton.karboviak@gmail.com>
2014-01-28 17:08:16 -0600

    First Commit CSci 230 HW #1
    first attempt at hw 2.  Still need to clarify if my code is correct.

---
+++ hw2/hw2-Karboviak.c
@@ -0,0 +1,14 @@
+/*
+ * File:   hw2-Karboviak.c
+ * Author: Kelton Karboviak
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	system("sort american0.txt american1.txt american2.txt > words.txt");
+
+ 	return 0;
+}
