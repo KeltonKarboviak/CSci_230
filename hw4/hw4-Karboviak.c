f40a3bc07b9b6212743b0604b2359951620ab02d
KeltonKarboviak <kelton.karboviak@gmail.com>
2014-02-13 00:25:01 -0600

    Second Commit HW #4
    finished parts a, b, & c completely.  More testing needs to be done.

--- hw4/hw4-Karboviak.c
+++ hw4/hw4-Karboviak.c
@@ -11,7 +11,7 @@
 typedef unsigned char uchar;
 
 
-int Load_File(char *fileName, uchar *sequence) {
+uchar* Load_File(char *fileName, int *size) {
 	FILE *inFile;
 	inFile = fopen(fileName, "rb");
 	if (inFile == NULL) {
@@ -19,19 +19,51 @@ int Load_File(char *fileName, uchar *sequence) {
 		exit (0);
 	}
 
+	// Determine File Size
 	uchar temp;
-	int count = 0;
+	*size = 0;
+	while (fscanf(inFile, "%c", &temp) == 1) {
+		if (temp == 'A'| temp == 'T' | temp == 'G' | temp == 'C')  (*size)++;
+	}
+
+	rewind(inFile);
+
+	// Create Dynamic uchar Array
+	uchar *sequence;
+	sequence = (uchar*) calloc(*size, sizeof(uchar));
+
+	// Fill Array With Sequence
+	*size = 0;
 	while (fscanf(inFile, "%c", &temp) == 1) {
 		if (temp == 'A'| temp == 'T' | temp == 'G' | temp == 'C') {
-			if (count % 70 == 0)  printf("\n");
-			printf("%c", temp);
-			count++;
+			sequence[*size] = temp;
+			(*size)++;
 		}
 	}
-	printf("\n\n");
 
 	fclose(inFile);
-	return 0;
+
+	return sequence;
+}
+
+
+int Search_Sequence(uchar *dna, uchar *fasta, int dLength, int fLength, int *start, int *end) {
+	int i, k;
+	for (i = 0; i < dLength; i++) {
+		k = 0;
+		while (1) {
+			if (i > dLength - fLength)   return -1;  // Last Point for Match
+			if (fasta[k] != dna[i + k])  break;
+			*start = i;
+			if (k == fLength - 1) {
+				*end = i + k;
+				return 0;
+			}
+			k++;
+		}
+	}
+
+	return -1;
 }
 
 
@@ -42,7 +74,10 @@ int main(int argc, char** argv) {
  ************************************************************\n\n";
 
 	// Displays Help Menu
-	if (!strcmp(argv[1], "?")) {
+	if (argc != 5 && argc != 2) {
+		printf("%s", error);
+		exit (0);
+	} else if (!strcmp(argv[1], "?")) {
 		printf("\n");
  		printf(" ************************************************************\n");
  		printf(" * HW4 Help Menu.                                           *\n");
@@ -56,9 +91,6 @@ int main(int argc, char** argv) {
  		printf(" *                                                          *\n");
 	 	printf(" ************************************************************\n");
 	 	printf("\n");
-		exit (0);
-	} else if (argc != 5) {
-		printf("%s", error);
 		exit (0);
 	}
 
@@ -79,13 +111,24 @@ int main(int argc, char** argv) {
 
 	char dnaFile[strlen(argv[1])];    strcpy(dnaFile, argv[1]);    // DNA Filename
 	char fastaFile[strlen(argv[2])];  strcpy(fastaFile, argv[2]);  // FASTA Filename
-	int forwardSize = atoi(argv[3]);   // Size of forward primer
-	int reverseSize = atoi(argv[4]);   // Size of reverse primer
+	int forwardSize = atoi(argv[3]);    // Size of forward primer
+	int reverseSize = atoi(argv[4]);    // Size of reverse primer
 
-	uchar *dnaArray, *fastaArray;      // Dynamic Arrays of Sequences
-	Load_File(dnaFile, dnaArray);      // Store DNA Sequence
-	Load_File(fastaFile, fastaArray);  // Store FASTA Sequence
+	uchar *dnaArray, *fastaArray;       // Dynamic Arrays of uchars
+	int dnaLength, fastaLength;         // Length of Files
+	dnaArray = Load_File(dnaFile, &dnaLength);        // Store DNA Sequence
+	fastaArray = Load_File(fastaFile, &fastaLength);  // Store FASTA Sequence
 
+	// Find the FASTA Sequence in DNA
+	int start, end;  // Index values of FASTA Sequence in DNA
+	if (Search_Sequence(dnaArray, fastaArray, dnaLength, fastaLength, &start, &end) == -1) {
+		printf("\nFASTA sequence was not found in DNA sequence.\n\n");
+		free(dnaArray);  free(fastaArray);
+		printf("%d %d\n", dnaLength, fastaLength);
+		printf("%d %d\n", start, end);
+		exit (0);
+	}
+	printf("%d %d\n", start, end);
 
 	free(dnaArray);  free(fastaArray);  // Free Dynamic Arrays
 	return 0;
