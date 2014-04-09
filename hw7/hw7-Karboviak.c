6679344e85594561c57b6743ec0e44e5e9966692
KeltonKarboviak <kelton.karboviak@gmail.com>
2014-04-09 00:45:13 -0500

    Second Commit HW #7
    Finished & submitted assignment

--- hw7/hw7-Karboviak.c
+++ hw7/hw7-Karboviak.c
@@ -31,7 +31,7 @@ typedef struct cNode _cNode;
 /* FUNCTIONS                                                */
 /************************************************************/
 void dInsert(_dNode *(*head), _dNode *(*tail), _dNode *(*newNode)) {
-    if (*head == NULL)  (*head) = (*tail) = (*newNode);
+    if ((*head) == NULL)  (*head) = (*tail) = (*newNode);
 
     (*tail)->next = (*newNode);
     (*tail) = (*newNode);
@@ -39,8 +39,9 @@ void dInsert(_dNode *(*head), _dNode *(*tail), _dNode *(*newNode)) {
 }
 
 
-void cInsert(_cNode **head, _cNode **tail, _cNode **newNode) {
-    if (*head == NULL)  (*head) = (*tail) = (*newNode);
+/*******************************************************************/
+void cInsert(_cNode *(*head), _cNode *(*tail), _cNode *(*newNode)) {
+    if ((*head) == NULL)  (*head) = (*tail) = (*newNode);
 
     (*tail)->next = (*newNode);
     (*tail) = (*newNode);
@@ -48,32 +49,55 @@ void cInsert(_cNode **head, _cNode **tail, _cNode **newNode) {
 }
 
 
-void dSearch() {
+/*******************************************************************/
+_dNode* dSearch(_dNode *(*head), _dNode *(*tail), char *searchWord) {
+	_dNode *temp = NULL;
 
+	temp = (*head);
+	while (temp != NULL) {
+		if (!strcmp(temp->word, searchWord))  return temp;
+
+		temp = temp->next;
+	}
+
+	return NULL;
 }
 
 
-void cSearch() {
+/*******************************************************************/
+_cNode* cSearch(_cNode *(*head), _cNode *(*tail), char *searchOrig, char *searchNew) {
+	_cNode *temp = NULL;
 
+	temp = (*head);
+	while (temp != NULL) {
+		if (!strcmp(temp->original, searchOrig) && !strcmp(temp->new, searchNew))  return temp;
+
+		temp = temp->next;
+	}
+
+	return NULL;
 }
 
 
+/*******************************************************************/
 void removeNode(_dNode *(*node)) {
 	_dNode *obsoleteNode = (_dNode*) malloc(sizeof(_dNode));
-	obsoleteNode = (*node)->next;
+	obsoleteNode  = (*node)->next;
 	(*node)->next = (*node)->next->next;
-	free(obsoleteNode->word);
-	free(obsoleteNode);
-	obsoleteNode = NULL;
+	free(obsoleteNode->word);  // Free word
+	free(obsoleteNode);        // Free pointer
+	obsoleteNode = NULL;       // Prevent dangling pointer
 }
 
 
+/*******************************************************************/
 void updateNode(_dNode *(*dNode), _cNode *(*cNode)) {
-	(*dNode)->word = (char*) malloc( (strlen( (*cNode)->new ) + 1) * sizeof(char));
+	(*dNode)->word = (char*) malloc( (strlen((*cNode)->new) + 1) * sizeof(char));
 	strcpy((*dNode)->word, (*cNode)->new);
 }
 
 
+/*******************************************************************/
 void correctPoem(_dNode *(*dHead), _dNode *(*dTail), _cNode *(*cHead), _cNode *(*cTail)) {
 	_dNode *dCurrent = (_dNode*) malloc(sizeof(_dNode));
 	_cNode *cCurrent = (_cNode*) malloc(sizeof(_cNode));
@@ -82,19 +106,23 @@ void correctPoem(_dNode *(*dHead), _dNode *(*dTail), _cNode *(*cHead), _cNode *(
 	cCurrent = (*cHead);
 	while (dCurrent != NULL && cCurrent != NULL) {
 		if (strcmp(cCurrent->original, cCurrent->new))  updateNode(&dCurrent, &cCurrent);
-		if (cCurrent->next != NULL && !strcmp(cCurrent->next->new, "!")) {
+		if (cCurrent->next != NULL && !strcmp(cCurrent->next->new,"!")) {
 			removeNode(&dCurrent);
 			cCurrent = cCurrent->next;
 		}
 
-
 		dCurrent = dCurrent->next;
 		cCurrent = cCurrent->next;
 	}
+
+	// Free both current pointers
+	free(dCurrent);  dCurrent = NULL;
+	free(cCurrent);  cCurrent = NULL;
 }
 
 
-void printPoem(_dNode **head) {
+/*******************************************************************/
+void printPoem(_dNode *(*head)) {
 	_dNode *current = (_dNode*) malloc(sizeof(_dNode));
 	current = (*head);
 	while (current != NULL) {
@@ -105,16 +133,46 @@ void printPoem(_dNode **head) {
 
 		current = current->next;
 	}
+
+	// Free current pointer
+	free(current);  current = NULL;
 }
 
 
+/*******************************************************************/
 void destroyDataList(_dNode *(*head), _dNode *(*tail), _dNode *(*current)) {
+	_dNode *temp = NULL;
 
+	while ((*head) != NULL) {
+		temp = (*head);
+		(*head) = (*head)->next;
+		free(temp->word);
+		free(temp);
+		temp = NULL;
+	}
+
+	// Free current, head & tail pointers
+	(*current) = (*head) = (*tail) = NULL;
+	free((*current));  free((*head));  free((*tail));
 }
 
 
+/*******************************************************************/
 void destroyCodexList(_cNode *(*head), _cNode *(*tail), _cNode *(*current)) {
+	_cNode *temp = NULL;
 
+	while ((*head) != NULL) {
+		temp = (*head);
+		(*head) = (*head)->next;
+		free(temp->original);
+		free(temp->new);
+		free(temp);
+		temp = NULL;
+	}
+
+	// Free current, head & tail pointers
+	(*current) = (*head) = (*tail) = NULL;
+	free((*current));  free((*head));  free((*tail));
 }
 
 
@@ -147,9 +205,10 @@ int main(void) {
         if (feof(dataFile)) break;
     }
 
-	// Free pointer
-	free(dCurrent);  dCurrent = NULL;
+	// Free temp pointer
+	free(temp);      temp = NULL;
     fclose(dataFile);
+
 
 
     // Open codexFile for reading
@@ -185,24 +244,24 @@ int main(void) {
         strcpy(cCurrent->new,      tempNew);
         cInsert(&cHead, &cTail, &cCurrent);
 
+		free(temp);  temp = NULL;
         if (feof(codexFile)) break;
     }
 
-	// Free pointers
-	free(temp);  free(tempOld);  //free(tempNew);  // ERROR ERROR ERROR ERROR ERROR ERROR
-	free(cCurrent); cCurrent = NULL;
+	// Free temp pointers
+	tempOld = tempNew = NULL;
+	free(tempOld);  free(tempNew);
     fclose(codexFile);
 
-	printPoem(&dHead);
-	printf("*******************************************\n");
+
 	// Correct the poem
 	correctPoem(&dHead, &dTail, &cHead, &cTail);
 
-	// Print the poem
+	// Print the final poem
 	printPoem(&dHead);
 
 
-
+	// Clean up Linked Lists
     destroyDataList(&dHead, &dTail, &dCurrent);
     destroyCodexList(&cHead, &cTail, &cCurrent);
     return 0;
